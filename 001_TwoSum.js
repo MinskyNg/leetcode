cf64fc95e4a1c8f2d3d2c1148e357444e743f03a
MinskyNg <minsky199525@gmail.com>
2016-09-14 23:02:43 +0800

    001 002

---
+++ 001_TwoSum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var hash = {};
+    for (var i = 0, len = nums.length; i < len; i++) {
+        if (hash[target - nums[i]] !== undefined) {
+            return [hash[target - nums[i]], i];
+        } else {
+            hash[nums[i]] = i;
+        }
+    }
+};
---
+++ 002_AddTwoNumbers.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    var result = [];
+    var sum, carry = 0;
+    do {
+        sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+        carry = (sum > 9) ? 1 : 0;
+        result.push(sum % 10);
+        l1 = l1 ? l1.next : 0;
+        l2 = l2 ? l2.next : 0;
+    } while (l1 || l2 || carry)
+    return result;
+}
